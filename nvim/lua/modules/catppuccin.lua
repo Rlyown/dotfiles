2fdb0d2e8baf87e89d3d18e4b9c24c547c88d8e4
Chaos Chen <czy426@icloud.com>
2022-07-18 20:16:13 +0800

    first commit

---
+++ nvim/lua/modules/catppuccin.lua
@@ -0,0 +1,30 @@
+local catppuccin_status_ok, catppuccin = pcall(require, "catppuccin")
+if not catppuccin_status_ok then
+	return
+end
+
+-- configure it
+catppuccin.setup({
+	transparent_background = false,
+	term_colors = true,
+	integrations = {
+		lsp_trouble = true,
+		nvimtree = {
+			enabled = true,
+			show_root = true,
+			transparent_panel = false,
+		},
+		which_key = true,
+		-- This option will override ts_rainbow colors in treesitter.lua
+		ts_rainbow = true,
+		dap = {
+			enabled = true,
+			enable_ui = true,
+		},
+	},
+	compile = {
+		enabled = true,
+		path = vim.fn.stdpath("config") .. "/plugin",
+		suffix = "_compiled",
+	},
+})
